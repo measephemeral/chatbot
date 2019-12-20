9339031b86ec4c7f009cd0b9343c880def2aa7d9
measephemeral <memejr0707@gmail.com>
2019-12-20 17:37:08 +0900

    메아봇 완성(임시)

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import requests
 from decouple import config
+import random
+import bs4
+import re
 
 app = Flask(__name__)
 
@@ -11,6 +14,8 @@ def hello():
 api_url = 'https://api.telegram.org'
 token = config('TOKEN')
 chat_id = config('CHAT_ID')
+naver_client_id = config('NAVER_CLIENT_ID')
+naver_client_secret = config('NAVER_CLIENT_SECRET')
 
 @app.route('/send/<text>')
 def send(text):
@@ -22,7 +27,58 @@ def chatbot():
     from_telegram = request.get_json()
     chat_id = from_telegram.get('message').get('from').get('id')
     text = from_telegram.get('message').get('text')
-    res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+    
+    #메뉴 추천하기
+    if text=='메뉴':
+        menus = ['양자강','20층','김밥카페']
+        lunch = random.choice(menus)
+        response = lunch
+    elif text=='메아':
+        response = '응.'
+    elif text=='이름':
+        response = '메아 S 에페메랄.'
+    elif text=='로또':
+        lotto = random.sample(range(1,46),6)
+        lotto = sorted(lotto)
+        response = f'추천 로또 번호는{lotto}야'
+    elif text[0:4]=='영번역 ':
+        to_be_translate = text[4:]
+        url='https://openapi.naver.com/v1/papago/n2mt'
+        headers = {
+            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Naver-Client-Id':naver_client_id,
+            'X-Naver-Client-Secret':naver_client_secret
+        }
+        data = f'source=ko&target=en&text={to_be_translate}'.encode('utf-8')
+        res = requests.post(url,headers=headers,data=data).json()
+        response = res.get('message').get('result').get('translatedText')
+    elif text[0:4]=='일번역 ':
+        to_be_translate = text[4:]
+        url='https://openapi.naver.com/v1/papago/n2mt'
+        headers = {
+            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Naver-Client-Id':naver_client_id,
+            'X-Naver-Client-Secret':naver_client_secret
+        }
+        data = f'source=ko&target=ja&text={to_be_translate}'.encode('utf-8')
+        res = requests.post(url,headers=headers,data=data).json()
+        response = res.get('message').get('result').get('translatedText')    
+
+    elif text=='날씨':
+        html = requests.get('https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09680101')
+
+        soup = bs4.BeautifulSoup(html.text,'html.parser')
+        weather = soup.select_one('#content > div.w_now2 > ul > li:nth-child(1) > div > em')
+        weather = str(weather)
+        weather = re.sub('<.+?>','',weather)
+        weather = re.sub('&nbsp;| |\t|\r|\n','',weather)
+        response = f'지금 서울 역삼동의 날씨는 {weather}야'
+    else:
+        response = f'{text} 가 무슨 말인지 모르겠어'
+
+
+
+    res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={response}')
     
     #print(request.get_json())
     return 'ok',200
