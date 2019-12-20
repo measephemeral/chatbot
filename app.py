6915974dbc0eb7030468d12c33f922881e7fbc64
measephemeral <memejr0707@gmail.com>
2019-12-20 14:21:45 +0900

    챗봇 생성

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import requests
+from decouple import config
 
 app = Flask(__name__)
 
@@ -8,18 +9,13 @@ def hello():
     return 'hello world!'
 
 api_url = 'https://api.telegram.org'
-token = '1008408942:AAE49DpolKuuVbJxkP5F-SAUWo_mA9m9vW0'
-chat_id = '958067137'
+token = config('TOKEN')
+chat_id = config('CHAT_ID')
 
 @app.route('/send/<text>')
 def send(text):
     res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return 'OK!'
 
-
-
-# 토큰 1008408942:AAE49DpolKuuVbJxkP5F-SAUWo_mA9m9vW0
-# https://api.telegram.org/bot<token>/METHOD_NAME
-# ID: 958067137
 
 app.run(debug=True)
