44db946fdddd598c0cccd5002bcec5d6c722ae81
measephemeral <memejr0707@gmail.com>
2019-12-20 14:35:27 +0900

    챗봇 진행중

--- app.py
+++ app.py
@@ -17,5 +17,10 @@ def send(text):
     res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return 'OK!'
 
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    return 'ok',200
+    # status code 200 -> ok! 잘 접수했다
+
 
 app.run(debug=True)
