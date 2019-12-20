67b8fc0e2d044cfb1d1dc483b3c09ea53a8194f2
measephemeral <memejr0707@gmail.com>
2019-12-20 15:33:32 +0900

    말 따라하기

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 from decouple import config
 
@@ -19,8 +19,15 @@ def send(text):
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
+    from_telegram = request.get_json()
+    chat_id = from_telegram.get('message').get('from').get('id')
+    text = from_telegram.get('message').get('text')
+    res = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+    
+    #print(request.get_json())
     return 'ok',200
     # status code 200 -> ok! 잘 접수했다
 
+# https://b0072674.ngrok.io
 
 app.run(debug=True)
